89f32d51376c68a4d2948e7713c8bf57cfbf8433
Opeyemi Oyelesi <oyelesiopeyemi@gmail.com>
2020-06-23 17:10:45 +0100

    Modified multiple_space_spec file

--- spec/multiple_space_spec.rb
+++ spec/multiple_space_spec.rb
@@ -1,4 +1,8 @@
-# require_relative '../lib/multiple_space.rb'
+require_relative '../lib/multiple_space.rb'
+
+
+include MultipleSpace
+include Colorize
 
 describe MultipleSpace do
     let(:file) { './style.css' }
@@ -10,8 +14,8 @@ describe MultipleSpace do
     it 'returns an empty array when all the colons have the right afterspace' do
     #   subject.multiple_space_check(file_details[2], 2, error_array)
 
-    multiple_space_check(file_details[34], 34, error_messages)
-      expect(error_messages).to be_a(Array)
+    multiple_space_check(file_details[34], 34, error_array)
+      expect(error_array).to be_a(Array)
         # file_details.each_with_index { |line, index| after_colon_check(line_content, line_index, error_messages) }end.to be_a_kind_of(Array)
         # expect(MultipleSpace.multiple_space_check(file_details[2], 2, error_array)).to return_a(Array)
     end
